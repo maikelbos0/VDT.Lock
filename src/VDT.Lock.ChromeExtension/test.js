3644dbcae1c8767ea303f0b5c35cc026f6ddc085
Maikel Bos <maikel.bos0@gmail.com>
2024-09-07 15:23:54 +0200

    Add test chrome extension

---
+++ src/VDT.Lock.ChromeExtension/test.js
@@ -0,0 +1,11 @@
+import { dotnet } from './wasm/dotnet.js'
+
+const { getAssemblyExports, getConfig } = await dotnet.create();
+const config = getConfig();
+const exports = await getAssemblyExports(config.mainAssemblyName);
+
+document.getElementById('test').addEventListener('click', function () {
+    const text = exports.VDT.Lock.TestClass.Test();
+
+    document.getElementById("result").innerText = text;
+});
