cf312698f906acc8db0058d94a59bff1a3beeeca
Maikel Bos <maikel.bos0@gmail.com>
2024-09-08 09:50:01 +0200

    Add hash provider implementation

---
+++ src/VDT.Lock.Tests/HashProviderTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Xunit;
+
+namespace VDT.Lock.Tests;
+
+public class HashProviderTests {
+    [Fact]
+    public void Provide() {
+        var expectedResult = new byte[] { 94, 127, 205, 144, 246, 91, 11, 57, 225, 92, 158, 62, 250, 193, 91, 110, 190, 217, 203, 200, 94, 121, 41, 154, 149, 62, 49, 26, 113, 1, 233, 151 };
+
+        var subject = new HashProvider();
+
+        using var stream = new MemoryStream();
+        var password = "password"u8.ToArray();
+        stream.Write(password, 0, password.Length);
+        stream.Seek(0, SeekOrigin.Begin);
+
+        var salt = new byte[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 };
+
+        var result = subject.Provide(stream, salt);
+
+        // We mostly care only that it's deterministic
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ src/VDT.Lock/HashProvider.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+
+namespace VDT.Lock;
+
+public sealed class HashProvider {
+    private const int iterations = 1_000_000;
+
+    public byte[] Provide(Stream stream, byte[] salt) {
+        using var sha = SHA256.Create();
+
+        var password = sha.ComputeHash(stream);
+
+        using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations, HashAlgorithmName.SHA256);
+
+        return pbkdf2.GetBytes(SHA256.HashSizeInBytes);
+    }
+}
