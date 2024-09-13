aff4a6bca887204b03b5dec988f2ced8778ef36e
Maikel Bos <maikel.bos0@gmail.com>
2024-09-13 14:02:34 +0200

    Rename input variable for consistency

--- src/VDT.Lock/HashProvider.cs
+++ src/VDT.Lock/HashProvider.cs
@@ -5,10 +5,10 @@ namespace VDT.Lock;
 public sealed class HashProvider : IHashProvider {
     public const int Iterations = 1_000_000;
 
-    public byte[] Provide(Stream stream, byte[] salt) {
+    public byte[] Provide(Stream inputStream, byte[] salt) {
         using var sha = SHA256.Create();
 
-        var password = sha.ComputeHash(stream);
+        var password = sha.ComputeHash(inputStream);
 
         using var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iterations, HashAlgorithmName.SHA256);
 
