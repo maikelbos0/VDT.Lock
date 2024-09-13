d464c2874ab6b11e31fd08d6c0364b08445e4eba
Maikel Bos <maikel.bos0@gmail.com>
2024-09-13 13:53:04 +0200

    Refactor to eliminate warnings and streamline C# encrypt implementation

--- src/VDT.Lock/Encryptor.cs
+++ src/VDT.Lock/Encryptor.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices.JavaScript;
+﻿#if BROWSER
+using System.Runtime.InteropServices.JavaScript;
+#endif
 using System.Security.Cryptography;
 
 namespace VDT.Lock;
@@ -13,10 +15,10 @@ public sealed partial class Encryptor {
         this.randomByteGenerator = randomByteGenerator;
     }
 
+#if BROWSER
     public async Task<byte[]> Encrypt(Stream inputStream, byte[] key) {
         var iv = randomByteGenerator.Generate(BlockSizeInBytes);
 
-#if BROWSER
         await JSHost.ImportAsync("Encryptor", "../encryptor.js");
 
         var memoryStream = new MemoryStream();
@@ -25,40 +27,47 @@ public sealed partial class Encryptor {
         var input = memoryStream.ToArray();
 
         var encryptedBytes = await Encrypt(input, key, iv) as byte[] ?? throw new InvalidOperationException();
-#else
-        using var aes = Aes.Create();
-        aes.Mode = CipherMode.CBC;
-
-        var encryptor = aes.CreateEncryptor(key, iv);
-        var encryptedStream = new MemoryStream();
-        using var cryptoStream = new CryptoStream(encryptedStream, encryptor, CryptoStreamMode.Write);
-
-        inputStream.CopyTo(cryptoStream);
-        cryptoStream.FlushFinalBlock();
-
-        var encryptedBytes = encryptedStream.ToArray();
-
-#endif
         
         var result = new byte[iv.Length +  encryptedBytes.Length];
         Buffer.BlockCopy(iv, 0, result, 0, iv.Length);
         Buffer.BlockCopy(encryptedBytes, 0, result, iv.Length, encryptedBytes.Length);
         return result;
     }
-
-    public Stream Decrypt(byte[] bytes, byte[] key) {
-        var iv = bytes.Take(BlockSizeInBytes).ToArray();
-
-        bytes = bytes.Skip(BlockSizeInBytes).ToArray();
-
+#else
+    public Task<byte[]> Encrypt(Stream inputStream, byte[] key) {
+        var iv = randomByteGenerator.Generate(BlockSizeInBytes);
+#pragma warning disable CA1416 // Validate platform compatibility
         using var aes = Aes.Create();
+#pragma warning restore CA1416 // Validate platform compatibility
+        aes.Mode = CipherMode.CBC;
+
+        var encryptor = aes.CreateEncryptor(key, iv);
+        var encryptedStream = new MemoryStream();
+        encryptedStream.Write(iv, 0, iv.Length);
+
+        using var cryptoStream = new CryptoStream(encryptedStream, encryptor, CryptoStreamMode.Write);
+
+        inputStream.CopyTo(cryptoStream);
+        cryptoStream.FlushFinalBlock();
+
+        return Task.FromResult(encryptedStream.ToArray());
+    }
+#endif
+
+    public Stream Decrypt(byte[] encryptedBytes, byte[] key) {
+        var iv = encryptedBytes.Take(BlockSizeInBytes).ToArray();
+
+        encryptedBytes = encryptedBytes.Skip(BlockSizeInBytes).ToArray();
+
+#pragma warning disable CA1416 // Validate platform compatibility
+        using var aes = Aes.Create();
+#pragma warning restore CA1416 // Validate platform compatibility
         aes.Mode = CipherMode.CBC;
 
         var decryptor = aes.CreateDecryptor(key, iv);
 
-        return new CryptoStream(new MemoryStream(bytes), decryptor, CryptoStreamMode.Read);
+        return new CryptoStream(new MemoryStream(encryptedBytes), decryptor, CryptoStreamMode.Read);
     }
-
 
 #if BROWSER
     [JSImport("Encrypt", "Encryptor")]
@@ -69,7 +78,4 @@ public sealed partial class Encryptor {
         [JSMarshalAs<JSType.Array<JSType.Number>>] byte[] iv
     );
 #endif
-
-    // TODO rename bytes/encryptedbytes = encryptedData / encryptedBytes?
-    // TODO rename stream = inputStream / plainDataStream?
 }
