5f0c03888a0e663b7043c7dd15d75e572a60b91f
Maikel Bos <maikel.bos0@gmail.com>
2024-09-14 09:42:59 +0200

    Add secure byte array class to replace streams

---
+++ src/VDT.Lock.Tests/SecureByteArrayTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace VDT.Lock.Tests;
+
+public sealed class SecureByteArrayTests {
+    [Fact]
+    public void Constructor() {
+        using var subject = new SecureByteArray(16);
+
+        Assert.Equal(16, subject.Buffer.Length);
+    }
+
+    [Fact]
+    public void Dispose() {
+        byte[] buffer;
+
+        using (var subject = new SecureByteArray(16)) {
+            for (var i = 0; i < 16; i++) {
+                subject.Buffer[i] = 255;
+            }
+
+            buffer = subject.Buffer;
+        }
+
+        Assert.Equal(new byte[16], buffer);
+    }
+}
---
+++ src/VDT.Lock/SecureByteArray.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace VDT.Lock;
+
+public sealed class SecureByteArray : IDisposable {
+    private readonly GCHandle bufferHandle;
+
+    public byte[] Buffer { get; }
+
+    public SecureByteArray(int length) {
+        Buffer = new byte[length];
+        bufferHandle = GCHandle.Alloc(Buffer, GCHandleType.Pinned);
+    }
+
+    public void Dispose() {
+        Zero();
+        bufferHandle.Free();
+    }
+
+    [MethodImpl(MethodImplOptions.NoOptimization)]
+    private void Zero() {
+        for (int i = 0; i < Buffer.Length; i++) {
+            Buffer[i] = 0;
+        }
+    }
+}
