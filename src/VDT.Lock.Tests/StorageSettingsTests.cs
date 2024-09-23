45ef91fad3e43752c4eac7e7f4db45f175f4a759
Maikel Bos <maikel.bos0@gmail.com>
2024-09-23 19:51:22 +0200

    Add StorageSettings with deserialization

---
+++ src/VDT.Lock.Tests/StorageSettingsTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+
+namespace VDT.Lock.Tests;
+
+public class StorageSettingsTests {
+    [Fact]
+    public void Constructor() {
+        var settingsSpan = new ReadOnlySpan<byte>([
+            3, 0, 0, 0, 98, 97, 114,
+            4, 0, 0, 0, 1, 2, 3, 4,
+            3, 0, 0, 0, 102, 111, 111,
+            5, 0, 0, 0, 5, 6, 7, 8, 9
+        ]);
+
+        using var subject = new StorageSettings(settingsSpan);
+
+        Assert.Equal(new ReadOnlySpan<byte>([1, 2, 3, 4]), subject.GetSetting("bar"));
+        Assert.Equal(new ReadOnlySpan<byte>([5, 6, 7, 8, 9]), subject.GetSetting("foo"));
+    }
+
+    [Fact]
+    public void Dispose() {
+        var settingsSpan = new ReadOnlySpan<byte>([
+            3, 0, 0, 0, 98, 97, 114,
+            4, 0, 0, 0, 1, 2, 3, 4,
+            3, 0, 0, 0, 102, 111, 111,
+            5, 0, 0, 0, 5, 6, 7, 8, 9
+        ]);
+        Dictionary<string, SecureBuffer> settings;
+        
+        using (var subject = new StorageSettings(settingsSpan)) {
+            var settingsField = typeof(StorageSettings).GetField("settings", BindingFlags.NonPublic | BindingFlags.Instance) ?? throw new InvalidOperationException();
+            settings = settingsField.GetValue(subject) as Dictionary<string, SecureBuffer> ?? throw new InvalidOperationException();
+        };
+
+        Assert.Equal(2, settings.Count);
+
+        foreach (var buffer in settings.Values) {
+            Assert.True(buffer.IsDisposed);
+        }
+    }
+}
---
+++ src/VDT.Lock/StorageSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace VDT.Lock;
+
+public sealed class StorageSettings : IDisposable {
+    private Dictionary<string, SecureBuffer> settings = [];
+
+    public StorageSettings(ReadOnlySpan<byte> settingsSpan) {
+        var position = 0;
+
+        while (position < settingsSpan.Length) {
+            settings.Add(settingsSpan.ReadString(ref position), settingsSpan.ReadSecureBuffer(ref position));
+        }
+    }
+
+    public ReadOnlySpan<byte> GetSetting(string key)
+        => new ReadOnlySpan<byte>(settings[key].Value);
+
+    public void Dispose() {
+        foreach (var value in settings.Values) {
+            value.Dispose();
+        }
+        GC.SuppressFinalize(this);
+    }
+}
