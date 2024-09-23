e9ca2d0c3b41920106eac231be0c7452e6e2f0ce
Maikel Bos <maikel.bos0@gmail.com>
2024-09-23 18:52:24 +0200

    Add extension methods for deserializing settings from buffer

---
+++ src/VDT.Lock.Tests/ReadOnlySpanExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+namespace VDT.Lock.Tests;
+
+public class ReadOnlySpanExtensionsTests {
+    [Fact]
+    public void ReadInt() {
+        var subject = new ReadOnlySpan<byte>([0, 0, 0, 0, 8, 4, 2, 1, 0, 0, 0, 0]);
+        var position = 4;
+
+        var result = subject.ReadInt(ref position);
+
+        Assert.Equal(8, position);
+        Assert.Equal(16909320, result);
+    }
+
+    [Fact]
+    public void ReadString() {
+
+        var subject = new ReadOnlySpan<byte>([0, 0, 0, 0, 3, 0, 0, 0, 97, 98, 99, 0]);
+        var position = 4;
+
+        var result = subject.ReadString(ref position);
+
+        Assert.Equal(11, position);
+        Assert.Equal("abc", result);
+    }
+}
---
+++ src/VDT.Lock/ReadOnlySpanExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace VDT.Lock;
+
+public static class ReadOnlySpanExtensions {
+    public static int ReadInt(this ReadOnlySpan<byte> span, ref int position) {
+        return span[position++] | (span[position++] << 8) | (span[position++] << 16) | (span[position++] << 24);
+    }
+
+    public static string ReadString(this ReadOnlySpan<byte> span, ref int position) {
+        var length = ReadInt(span, ref position);
+        var value = Encoding.UTF8.GetString(span.Slice(position, length));
+
+        position += length;
+
+        return value;
+    }
+}
