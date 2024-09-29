86166f45ad162fa553249727acf335643c6fc3f5
Maikel Bos <maikel.bos0@gmail.com>
2024-09-29 13:19:52 +0200

    Add DataField class as part of eventual DataStore

---
+++ src/VDT.Lock.Tests/DataFieldTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Xunit;
+
+namespace VDT.Lock.Tests; 
+
+public class DataFieldTests {
+    [Fact]
+    public void Constructor() {
+        var plainSpan = new ReadOnlySpan<byte>([
+            3, 0, 0, 0, 98, 97, 114,
+            5, 0, 0, 0, 5, 6, 7, 8, 9
+        ]);
+
+        using var subject = new DataField(plainSpan);
+
+        Assert.Equal(new ReadOnlySpan<byte>([98, 97, 114]), subject.Name);
+        Assert.Equal(new ReadOnlySpan<byte>([5, 6, 7, 8, 9]), subject.Data);
+    }
+
+    [Fact]
+    public void SetName() {
+        var plainSpan = new ReadOnlySpan<byte>([
+            3, 0, 0, 0, 98, 97, 114,
+            5, 0, 0, 0, 5, 6, 7, 8, 9
+        ]);
+
+        using var subject = new DataField(plainSpan);
+
+        var plainPreviousValueBuffer = subject.GetBuffer("plainNameBuffer");
+
+        subject.Name = new ReadOnlySpan<byte>([99, 99, 99]);
+
+        Assert.Equal(new ReadOnlySpan<byte>([99, 99, 99]), subject.Name);
+        Assert.True(plainPreviousValueBuffer.IsDisposed);
+    }
+    
+    [Fact]
+    public void SetData() {
+        var plainSpan = new ReadOnlySpan<byte>([
+            3, 0, 0, 0, 98, 97, 114,
+            5, 0, 0, 0, 5, 6, 7, 8, 9
+        ]);
+
+        using var subject = new DataField(plainSpan);
+
+        var plainPreviousValueBuffer = subject.GetBuffer("plainDataBuffer");
+
+        subject.Data = new ReadOnlySpan<byte>([99, 99, 99]);
+
+        Assert.Equal(new ReadOnlySpan<byte>([99, 99, 99]), subject.Data);
+        Assert.True(plainPreviousValueBuffer.IsDisposed);
+    }
+    
+    [Fact]
+    public void Dispose() {
+        var plainSpan = new ReadOnlySpan<byte>([
+            3, 0, 0, 0, 98, 97, 114,
+            5, 0, 0, 0, 5, 6, 7, 8, 9
+        ]);
+        SecureBuffer plainNameBuffer;
+        SecureBuffer plainDataBuffer;
+
+        using (var subject = new DataField(plainSpan)) {
+            plainNameBuffer = subject.GetBuffer("plainNameBuffer");
+            plainDataBuffer = subject.GetBuffer("plainDataBuffer");
+        };
+
+        Assert.True(plainNameBuffer.IsDisposed);
+        Assert.True(plainDataBuffer.IsDisposed);
+    }
+}
---
+++ src/VDT.Lock/DataField.cs
@@ -0,0 +1,40 @@
+﻿namespace VDT.Lock;
+
+public sealed class DataField : IDisposable {
+    private SecureBuffer plainNameBuffer;
+    private readonly object plainNameBufferLock = new();
+    private SecureBuffer plainDataBuffer;
+    private readonly object plainDataBufferLock = new();
+
+    public ReadOnlySpan<byte> Name {
+        get => new(plainNameBuffer.Value);
+        set {
+            lock (plainNameBufferLock) {
+                plainNameBuffer.Dispose();
+                plainNameBuffer = new(value.ToArray());
+            }
+        }
+    }
+    public ReadOnlySpan<byte> Data {
+        get => new(plainDataBuffer.Value);
+        set {
+            lock (plainDataBufferLock) {
+                plainDataBuffer.Dispose();
+                plainDataBuffer = new(value.ToArray());
+            }
+        }
+    }
+
+    public DataField(ReadOnlySpan<byte> plainSpan) {
+        var position = 0;
+
+        plainNameBuffer = plainSpan.ReadSecureBuffer(ref position);
+        plainDataBuffer = plainSpan.ReadSecureBuffer(ref position);
+    }
+
+    public void Dispose() {
+        plainNameBuffer.Dispose();
+        plainDataBuffer.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
