2d7dc37c26be33b95ea2fbc1ea17c51028601235
Maikel Bos <maikel.bos0@gmail.com>
2024-10-04 20:13:57 +0200

    Add a collection that disposes items

---
+++ src/VDT.Lock.Tests/DataCollectionTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace VDT.Lock.Tests;
+
+public class DataCollectionTests {
+    public class TestDisposable : IDisposable {
+        public bool IsDisposed { get; private set; }
+
+        public void Dispose() {
+            IsDisposed = true;
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    [Fact]
+    public void Count() {
+        using var subject = new DataCollection<TestDisposable>();
+
+        subject.Add();
+
+        Assert.Equal(1, subject.Count);
+    }
+
+    [Fact]
+    public void Add() {
+        using var subject = new DataCollection<TestDisposable>();
+
+        var result = subject.Add();
+
+        Assert.Equal(result, Assert.Single(subject));
+    }
+
+    [Fact]
+    public void Dispose() {
+        var items = new List<TestDisposable>();
+
+        using (var subject = new DataCollection<TestDisposable>()) {
+            items.Add(subject.Add());
+            items.Add(subject.Add());
+        };
+
+        Assert.Equal(2, items.Count);
+
+        foreach (var item in items) {
+            Assert.True(item.IsDisposed);
+        }
+    }
+}
---
+++ src/VDT.Lock/DataCollection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VDT.Lock;
+
+public sealed class DataCollection<T> : IDisposable, IEnumerable<T> where T : notnull, IDisposable, new() {
+    private readonly List<T> items = [];
+
+    public int Count => items.Count;
+
+    public T Add() {
+        var item = new T();
+        items.Add(item);
+        return item;
+    }
+
+    public IEnumerator<T> GetEnumerator() => items.GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => items.GetEnumerator();
+
+    public void Dispose() {
+        foreach (var item in items) {
+            item.Dispose();
+        }
+        GC.SuppressFinalize(this);
+    }
+}
