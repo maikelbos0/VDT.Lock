12eb1cae02645460430a43fdb33efc5c964d4471
Maikel Bos <maikel.bos0@gmail.com>
2024-10-13 09:54:39 +0200

    Add DataStore

---
+++ src/VDT.Lock.Tests/DataStoreTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Xunit;
+
+namespace VDT.Lock.Tests;
+
+public class DataStoreTests {
+    [Fact]
+    public void Constructor() {
+        using var subject = new DataStore([98, 97, 114]);
+
+        Assert.Equal(new ReadOnlySpan<byte>([98, 97, 114]), subject.Name);
+    }
+
+    [Fact]
+    public void SetName() {
+        using var subject = new DataStore();
+
+        var plainPreviousValueBuffer = subject.GetBuffer("plainNameBuffer");
+
+        subject.Name = new ReadOnlySpan<byte>([99, 99, 99]);
+
+        Assert.Equal(new ReadOnlySpan<byte>([99, 99, 99]), subject.Name);
+        Assert.True(plainPreviousValueBuffer.IsDisposed);
+    }
+
+    [Fact]
+    public void Dispose() {
+        SecureBuffer plainNameBuffer;
+        DataCollection<DataItem> items;
+
+        using (var subject = new DataStore()) {
+            plainNameBuffer = subject.GetBuffer("plainNameBuffer");
+            items = subject.Items;
+        }
+
+        Assert.True(plainNameBuffer.IsDisposed);
+        Assert.True(items.IsDisposed);
+    }
+
+    [Fact]
+    public void GetNameThrowsIfDisposed() {
+        DataStore disposedSubject;
+
+        using (var subject = new DataStore()) {
+            disposedSubject = subject;
+        };
+
+        Assert.Throws<ObjectDisposedException>(() => { var _ = disposedSubject.Name; });
+    }
+
+    [Fact]
+    public void SetNameThrowsIfDisposed() {
+        DataStore disposedSubject;
+
+        using (var subject = new DataStore()) {
+            disposedSubject = subject;
+        };
+
+        Assert.Throws<ObjectDisposedException>(() => disposedSubject.Name = new ReadOnlySpan<byte>([15, 15, 15]));
+    }
+
+    [Fact]
+    public void ItemsThrowsIfDisposed() {
+        DataStore disposedSubject;
+
+        using (var subject = new DataStore()) {
+            disposedSubject = subject;
+        }
+
+        Assert.Throws<ObjectDisposedException>(() => disposedSubject.Items);
+    }
+}
---
+++ src/VDT.Lock/DataStore.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace VDT.Lock;
+
+public sealed class DataStore : IData, IDisposable {
+    private SecureBuffer plainNameBuffer;
+    private readonly DataCollection<DataItem> items = [];
+
+    public bool IsDisposed { get; private set; }
+
+    public ReadOnlySpan<byte> Name {
+        get {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            return new(plainNameBuffer.Value);
+        }
+        set {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            plainNameBuffer.Dispose();
+            plainNameBuffer = new(value.ToArray());
+        }
+    }
+
+    public DataCollection<DataItem> Items {
+        get {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            return items;
+        }
+    }
+
+    public int Length => throw new NotImplementedException();
+
+    public DataStore() : this(ReadOnlySpan<byte>.Empty) { }
+
+    public DataStore(ReadOnlySpan<byte> plainValueSpan) {
+        plainNameBuffer = new(plainValueSpan.ToArray());
+    }
+
+    public void SerializeTo(SecureByteList plainBytes) {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose() {
+        plainNameBuffer.Dispose();
+        items.Dispose();
+        IsDisposed = true;
+        GC.SuppressFinalize(this);
+    }
+}
