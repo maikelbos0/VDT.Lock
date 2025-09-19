3194c995b7a069169acbabdb1bd0a742d75c02dc
Maikel Bos <maikel.bos0@gmail.com>
2025-09-19 10:59:04 +0200

    Add splitter for chrome sync storage

---
+++ src/VDT.Lock/SecureBufferSplitter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VDT.Lock;
+
+public class SecureBufferSplitter {
+    private readonly SecureBuffer buffer;
+    private readonly int sectionSize;
+
+    public int SectionCount { get; }
+
+    public SecureBufferSplitter(SecureBuffer buffer, int sectionSize) {
+        this.buffer = buffer;
+        this.sectionSize = sectionSize;
+        SectionCount = (this.buffer.Value.Length - 1) / sectionSize + 1;
+    }
+
+    public byte[] GetHeader() {
+        unchecked {
+            return [(byte)SectionCount, (byte)(SectionCount >> 8), (byte)(SectionCount >> 16), (byte)(SectionCount >> 24)];
+        }
+    }
+
+    public ReadOnlySpan<byte> GetSection(int sectionIndex)
+        => new(buffer.Value, sectionIndex * sectionSize, Math.Min(sectionSize, buffer.Value.Length - sectionIndex * sectionSize));
+}
