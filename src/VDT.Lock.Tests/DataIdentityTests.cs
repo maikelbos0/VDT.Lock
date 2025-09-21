e6e3aeecce5f1e5521f510b7d83b24a0b92a66e3
Maikel Bos <maikel.bos0@gmail.com>
2025-09-21 10:42:12 +0200

    Add identity class

---
+++ src/VDT.Lock.Tests/DataIdentityTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Xunit;
+
+namespace VDT.Lock.Tests;
+
+public class DataIdentityTests {
+    [Fact]
+    public void DeserializeFrom() {
+        var plainSpan = new ReadOnlySpan<byte>([16, 0, 0, 0, 56, 240, 157, 219, 241, 61, 91, 71, 186, 251, 45, 225, 99, 172, 214, 4, 8, 0, 0, 0, 226, 189, 189, 101, 0, 0, 0, 0]);
+
+        using var subject = DataIdentity.DeserializeFrom(plainSpan);
+
+        Assert.Equal(new ReadOnlySpan<byte>([56, 240, 157, 219, 241, 61, 91, 71, 186, 251, 45, 225, 99, 172, 214, 4]), subject.Key);
+        Assert.Equal(new ReadOnlySpan<byte>([226, 189, 189, 101, 0, 0, 0, 0]), subject.Version);
+    }
+
+    [Fact]
+    public void EmptyConstructor() {
+        using var subject = new DataIdentity();
+
+        Assert.Equal(16, subject.Key.Length);
+        Assert.Equal(8, subject.Version.Length);
+    }
+
+    [Fact]
+    public void FieldLengths() {
+        using var subject = new DataIdentity();
+
+        Assert.Equal([16, 8], subject.FieldLengths);
+    }
+
+    [Fact]
+    public void SerializeTo() {
+        using var subject = new DataIdentity();
+
+        using var result = new SecureByteList();
+        subject.SerializeTo(result);
+
+        Assert.Equal(new ReadOnlySpan<byte>([32, 0, 0, 0, 16, 0, 0, 0, .. subject.Key, 8, 0, 0, 0, .. subject.Version]), result.GetValue());
+
+    }
+
+    [Fact]
+    public void Dispose() {
+        SecureBuffer plainKeyBuffer;
+        SecureBuffer plainVersionBuffer;
+
+        using (var subject = new DataIdentity()) {
+            plainKeyBuffer = subject.GetBuffer("plainKeyBuffer");
+            plainVersionBuffer = subject.GetBuffer("plainVersionBuffer");
+        }
+
+        Assert.True(plainKeyBuffer.IsDisposed);
+        Assert.True(plainVersionBuffer.IsDisposed);
+    }
+
+    [Fact]
+    public void IsDisposed() {
+        DataIdentity disposedSubject;
+
+        using (var subject = new DataIdentity()) {
+            disposedSubject = subject;
+        }
+
+        Assert.True(disposedSubject.IsDisposed);
+    }
+
+    [Fact]
+    public void KeyThrowsIfDisposed() {
+        DataIdentity disposedSubject;
+
+        using (var subject = new DataIdentity()) {
+            disposedSubject = subject;
+        }
+
+        Assert.Throws<ObjectDisposedException>(() => { var _ = disposedSubject.Key; });
+    }
+
+    [Fact]
+    public void VersionThrowsIfDisposed() {
+        DataIdentity disposedSubject;
+
+        using (var subject = new DataIdentity()) {
+            disposedSubject = subject;
+        }
+
+        Assert.Throws<ObjectDisposedException>(() => { var _ = disposedSubject.Version; });
+    }
+
+    [Fact]
+    public void FieldLengthsThrowsIfDisposed() {
+        DataIdentity disposedSubject;
+
+        using (var subject = new DataIdentity()) {
+            disposedSubject = subject;
+        }
+
+        Assert.Throws<ObjectDisposedException>(() => disposedSubject.FieldLengths);
+    }
+
+    [Fact]
+    public void SerializeToThrowsIfDisposed() {
+        DataIdentity disposedSubject;
+        using var plainBytes = new SecureByteList();
+
+        using (var subject = new DataIdentity()) {
+            disposedSubject = subject;
+        }
+
+        Assert.Throws<ObjectDisposedException>(() => disposedSubject.SerializeTo(plainBytes));
+    }
+}
---
+++ src/VDT.Lock/DataIdentity.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VDT.Lock;
+
+public sealed class DataIdentity : IData<DataIdentity>, IDisposable {
+    public static DataIdentity DeserializeFrom(ReadOnlySpan<byte> plainSpan) {
+        var position = 0;
+        
+        return new(plainSpan.ReadSpan(ref position), plainSpan.ReadSpan(ref position));
+    }
+    
+    private SecureBuffer plainKeyBuffer;
+    private SecureBuffer plainVersionBuffer;
+
+    public bool IsDisposed { get; private set; }
+
+    public ReadOnlySpan<byte> Key {
+        get {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            return new(plainKeyBuffer.Value);
+        }
+    }
+
+    public ReadOnlySpan<byte> Version {
+        get {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            return new(plainVersionBuffer.Value);
+        }
+    }
+
+    public IEnumerable<int> FieldLengths {
+        get {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            return [plainKeyBuffer.Value.Length, plainVersionBuffer.Value.Length];
+        }
+    }
+
+    public DataIdentity() {
+        plainKeyBuffer = new(Guid.NewGuid().ToByteArray());
+        plainVersionBuffer = new(DateTimeOffset.UtcNow.ToVersion());
+    }
+
+    private DataIdentity(ReadOnlySpan<byte> plainKeySpan, ReadOnlySpan<byte> plainVersionSpan) {
+        plainKeyBuffer = new(plainKeySpan.ToArray());
+        plainVersionBuffer = new(plainVersionSpan.ToArray());
+    }
+
+    public void SerializeTo(SecureByteList plainBytes) {
+        ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+        plainBytes.WriteInt(this.GetLength());
+        plainBytes.WriteSecureBuffer(plainKeyBuffer);
+        plainBytes.WriteSecureBuffer(plainVersionBuffer);
+    }
+
+    public void Dispose() {
+        plainKeyBuffer.Dispose();
+        plainVersionBuffer.Dispose();
+        IsDisposed = true;
+        GC.SuppressFinalize(this);
+    }
+}
