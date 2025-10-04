346df182223fdf2bf6747efa1e037297826fec19
Maikel Bos <maikel.bos0@gmail.com>
2025-10-04 15:46:38 +0200

    Add service for hashing secrets with test

---
+++ src/VDT.Lock.Api.Tests/Services/SecretHasherTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using VDT.Lock.Api.Services;
+using Xunit;
+
+namespace VDT.Lock.Api.Tests.Services;
+
+public class SecretHasherTests {
+    [Fact]
+    public void HashSecret() {
+        var secret = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+
+        var subject = new SecretHasher();
+
+        var (salt, hash) = subject.HashSecret(secret);
+
+        var expectedHash = Rfc2898DeriveBytes.Pbkdf2(secret, salt, SecretHasher.Iterations, HashAlgorithmName.SHA512, SHA512.HashSizeInBytes);
+
+        Assert.Equal(expectedHash, hash);
+    }
+}
---
+++ src/VDT.Lock.Api/Services/SecretHasher.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+
+namespace VDT.Lock.Api.Services;
+
+public sealed class SecretHasher {
+    public const int SaltSize = 16;
+    public const int Iterations = 1_000_000;
+
+    public (byte[] salt, byte[] hash) HashSecret(byte[] secret) {
+        var salt = RandomNumberGenerator.GetBytes(SaltSize);
+        var hash = Rfc2898DeriveBytes.Pbkdf2(secret, salt, Iterations, HashAlgorithmName.SHA512, SHA512.HashSizeInBytes);
+        
+        return (salt, hash);
+    }
+}
