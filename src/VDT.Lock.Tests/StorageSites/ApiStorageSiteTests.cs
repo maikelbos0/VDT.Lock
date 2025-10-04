e95d222e6f9b7282af82e73e0ef35347b399ba87
Maikel Bos <maikel.bos0@gmail.com>
2025-10-04 08:39:17 +0200

    Add API  storage site

---
+++ src/VDT.Lock.Tests/StorageSites/ApiStorageSiteTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using VDT.Lock.StorageSites;
+using Xunit;
+
+namespace VDT.Lock.Tests.StorageSites;
+
+public class ApiStorageSiteTests {
+    [Fact]
+    public void DeserializeFrom() {
+        var result = ApiStorageSite.DeserializeFrom(new ReadOnlySpan<byte>([4, 0, 0, 0, 110, 97, 109, 101, 8, 0, 0, 0, 108, 111, 99, 97, 116, 105, 111, 110, 2, 0, 0, 0, 105, 100, 6, 0, 0, 0, 115, 101, 99, 114, 101, 116]));
+
+        Assert.Equal(new ReadOnlySpan<byte>([110, 97, 109, 101]), result.Name);
+        Assert.Equal(new ReadOnlySpan<byte>([108, 111, 99, 97, 116, 105, 111, 110]), result.Location);
+        Assert.Equal(new ReadOnlySpan<byte>([105, 100]), result.DataStoreId);
+        Assert.Equal(new ReadOnlySpan<byte>([115, 101, 99, 114, 101, 116]), result.Secret);
+    }
+
+    [Fact]
+    public void Constructor() {
+        var plainNameSpan = new ReadOnlySpan<byte>([110, 97, 109, 101]);
+        var plainLocationSpan = new ReadOnlySpan<byte>([108, 111, 99, 97, 116, 105, 111, 110]);
+        var plainDataStoreIdSpan = new ReadOnlySpan<byte>([105, 100]);
+        var plainSecretSpan = new ReadOnlySpan<byte>([115, 101, 99, 114, 101, 116]);
+
+        using var subject = new ApiStorageSite(plainNameSpan, plainLocationSpan, plainDataStoreIdSpan, plainSecretSpan);
+
+        Assert.Equal(plainNameSpan, subject.Name);
+        Assert.Equal(plainLocationSpan, subject.Location);
+        Assert.Equal(plainDataStoreIdSpan, subject.DataStoreId);
+        Assert.Equal(plainSecretSpan, subject.Secret);
+    }
+
+    [Fact]
+    public void FieldLengths() {
+        using var subject = new ApiStorageSite([110, 97, 109, 101], [108, 111, 99, 97, 116, 105, 111, 110], [105, 100], [115, 101, 99, 114, 101, 116]);
+
+        Assert.Equal([0, 4, 8, 2, 6], subject.FieldLengths);
+    }
+
+    [Fact]
+    public void SerializeTo() {
+        using var subject = new ApiStorageSite([110, 97, 109, 101], [108, 111, 99, 97, 116, 105, 111, 110], [105, 100], [115, 101, 99, 114, 101, 116]);
+
+        using var result = new SecureByteList();
+        subject.SerializeTo(result);
+
+        Assert.Equal(new ReadOnlySpan<byte>([40, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 110, 97, 109, 101, 8, 0, 0, 0, 108, 111, 99, 97, 116, 105, 111, 110, 2, 0, 0, 0, 105, 100, 6, 0, 0, 0, 115, 101, 99, 114, 101, 116]), result.GetValue());
+    }
+    
+    [Fact]
+    public void GetLocationThrowsIfDisposed() {
+        ApiStorageSite subject;
+
+        using (subject = new([], [])) { }
+
+        Assert.Throws<ObjectDisposedException>(() => { var _ = subject.Location; });
+    }
+
+    [Fact]
+    public void SetLocationThrowsIfDisposed() {
+        ApiStorageSite subject;
+
+        using (subject = new([], [])) { }
+
+        Assert.Throws<ObjectDisposedException>(() => subject.Location = new ReadOnlySpan<byte>([108, 111, 99, 97, 116, 105, 111, 110]));
+    }
+
+    [Fact]
+    public void GetDataStoreIdThrowsIfDisposed() {
+        ApiStorageSite subject;
+
+        using (subject = new([], [])) { }
+
+        Assert.Throws<ObjectDisposedException>(() => { var _ = subject.DataStoreId; });
+    }
+
+    [Fact]
+    public void SetDataStoreIdThrowsIfDisposed() {
+        ApiStorageSite subject;
+
+        using (subject = new([], [])) { }
+
+        Assert.Throws<ObjectDisposedException>(() => subject.DataStoreId = new ReadOnlySpan<byte>([105, 100]));
+    }
+
+    [Fact]
+    public void GetSecretThrowsIfDisposed() {
+        ApiStorageSite subject;
+
+        using (subject = new([], [])) { }
+
+        Assert.Throws<ObjectDisposedException>(() => { var _ = subject.Secret; });
+    }
+
+    [Fact]
+    public void SetSecretThrowsIfDisposed() {
+        ApiStorageSite subject;
+
+        using (subject = new([], [])) { }
+
+        Assert.Throws<ObjectDisposedException>(() => subject.Secret = new ReadOnlySpan<byte>([115, 101, 99, 114, 101, 116]));
+    }
+
+    [Fact]
+    public void SerializeToThrowsIfDisposed() {
+        ApiStorageSite subject;
+        using var plainBytes = new SecureByteList();
+
+        using (subject = new([], [])) { }
+
+        Assert.Throws<ObjectDisposedException>(() => subject.SerializeTo(plainBytes));
+    }
+}
---
+++ src/VDT.Lock/StorageSites/ApiStorageSite.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace VDT.Lock.StorageSites;
+
+public class ApiStorageSite : StorageSiteBase {
+    public const int TypeId = 2;
+
+    public static new ApiStorageSite DeserializeFrom(ReadOnlySpan<byte> plainSpan) {
+        var position = 0;
+
+        return new(plainSpan.ReadSpan(ref position), plainSpan.ReadSpan(ref position), plainSpan.ReadSpan(ref position), plainSpan.ReadSpan(ref position));
+    }
+
+    protected SecureBuffer plainLocationBuffer;
+    protected SecureBuffer plainDataStoreIdBuffer;
+    protected SecureBuffer plainSecretBuffer;
+
+    public ReadOnlySpan<byte> Location {
+        get {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            return new(plainLocationBuffer.Value);
+        }
+        set {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            plainLocationBuffer.Dispose();
+            plainLocationBuffer = new(value.ToArray());
+        }
+    }
+
+    public ReadOnlySpan<byte> DataStoreId {
+        get {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            return new(plainDataStoreIdBuffer.Value);
+        }
+        set {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            plainDataStoreIdBuffer.Dispose();
+            plainDataStoreIdBuffer = new(value.ToArray());
+        }
+    }
+
+    public ReadOnlySpan<byte> Secret {
+        get {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            return new(plainSecretBuffer.Value);
+        }
+        set {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            plainSecretBuffer.Dispose();
+            plainSecretBuffer = new(value.ToArray());
+        }
+    }
+
+    public override IEnumerable<int> FieldLengths {
+        get {
+            ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+            return [0, plainNameBuffer.Value.Length, plainLocationBuffer.Value.Length, plainDataStoreIdBuffer.Value.Length, plainSecretBuffer.Value.Length];
+        }
+    }
+
+    public ApiStorageSite(ReadOnlySpan<byte> plainNameSpan, ReadOnlySpan<byte> location) : this(plainNameSpan, location, [], []) { }
+
+    public ApiStorageSite(ReadOnlySpan<byte> plainNameSpan, ReadOnlySpan<byte> location, ReadOnlySpan<byte> dataStoreId, ReadOnlySpan<byte> secret) : base(plainNameSpan) {
+        plainLocationBuffer = new(location.ToArray());
+        plainDataStoreIdBuffer = new(dataStoreId.ToArray());
+        plainSecretBuffer = new(secret.ToArray());
+    }
+
+    protected override Task<SecureBuffer?> ExecuteLoad() {
+        // If first time: request new storage with secret
+        //      -> API generates and returns id
+        // Store id + secret
+
+        // Request data using id + passcode
+        throw new NotImplementedException();
+    }
+
+    protected override Task<bool> ExecuteSave(SecureBuffer encryptedSpan) {
+        // If first time: request new storage with secret
+        //      -> API generates and returns id
+        // Store id + secret
+
+        // Save data using id + secret
+        throw new NotImplementedException();
+    }
+
+    public override void SerializeTo(SecureByteList plainBytes) {
+        ObjectDisposedException.ThrowIf(IsDisposed, this);
+
+        plainBytes.WriteInt(this.GetLength());
+        plainBytes.WriteInt(TypeId);
+        plainBytes.WriteSecureBuffer(plainNameBuffer);
+        plainBytes.WriteSecureBuffer(plainLocationBuffer);
+        plainBytes.WriteSecureBuffer(plainDataStoreIdBuffer);
+        plainBytes.WriteSecureBuffer(plainSecretBuffer);
+    }
+}
