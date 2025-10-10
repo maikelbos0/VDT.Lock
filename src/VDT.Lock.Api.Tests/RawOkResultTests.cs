50651fc35cef9f4df1db87863ccbead86e9d5b3a
Maikel Bos <maikel.bos0@gmail.com>
2025-10-10 14:15:11 +0200

    Switch to more specific type - we can rely on the standard result types for most scenarios

---
+++ src/VDT.Lock.Api.Tests/RawOkResultTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using NSubstitute;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VDT.Lock.Api.Tests;
+
+public class RawOkResultTests {
+    [Fact]
+    public async Task ExecuteAsyncWithData() {
+        var value = new byte[] { 0, 1, 2, 3, 4, 5, 6, 7 };
+        var subject = new RawOkResult(value);
+
+        var httpContext = Substitute.For<HttpContext>();
+        var body = Substitute.For<Stream>();
+        httpContext.Response.Body.Returns(body);
+
+        await subject.ExecuteAsync(httpContext);
+
+        httpContext.Response.Received().StatusCode = StatusCodes.Status200OK;
+        await body.Received().WriteAsync(value, default);
+    }
+}
