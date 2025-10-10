0b74c870fac4aa5a527ae9f8a8f0af5d7273a1e6
Maikel Bos <maikel.bos0@gmail.com>
2025-10-10 13:56:19 +0200

    Add custom result to be able to write raw bytes easier

---
+++ src/VDT.Lock.Api/Result.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace VDT.Lock.Api;
+
+public sealed record Result(int StatusCode, byte[]? Data) : IResult, IStatusCodeHttpResult {
+    int? IStatusCodeHttpResult.StatusCode => StatusCode;
+
+    public Result(int statusCode) : this(statusCode, null) { }
+
+    public async Task ExecuteAsync(HttpContext httpContext) {
+        ArgumentNullException.ThrowIfNull(httpContext);
+
+        httpContext.Response.StatusCode = StatusCode;
+
+        if (Data != null) {
+            await httpContext.Response.Body.WriteAsync(Data);
+        }
+    }
+}
